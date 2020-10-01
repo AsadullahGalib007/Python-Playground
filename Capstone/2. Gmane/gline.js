3350c5cc998515876c9786ef41c630dd90a1a8ea
AsadullahGalib007 <abrgalib@gmail.com>
2020-10-02 03:25:57 +0600

    Advanced Crawling

---
+++ Capstone/2. Gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','indiana.edu','berkeley.edu','ufp.pt','uct.ac.za','columbia.edu','mac.com','gmail.com','virginia.edu'],
+['2005-12',57,11,12,12,10,14,13,12,10,9],
+['2006-01',114,37,32,33,32,27,28,14,23,21],
+['2006-02',10,1,4,3,3,2,1,11,4,5]
+];
